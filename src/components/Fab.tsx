92fd4298e43a660ae8bfc871c4da2efd9a855c70
Damian Pinedo <DamianMorganti16@hotmail.com>
2021-05-27 12:05:33 -0400

    se agrego las funciones ios y android para el correcto funcionamiento en una u otra plataforma

--- src/components/Fab.tsx
+++ src/components/Fab.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { View, TouchableOpacity, Text, TouchableNativeFeedback } from 'react-native';
+import { View, TouchableOpacity, Text, TouchableNativeFeedback, Platform } from 'react-native';
 import { styles } from '../screens/styles/CounterScreenStyle';
 
 interface Props{
+
     title:string
     position:'br'|'bl';//si ponemos position? ese prop ya es opcional
 
@@ -11,6 +12,29 @@ interface Props{
 //en los parametros es lo mismo que poner (props:Props)
 export const Fab = ({title,onPress,position}:Props) => {
     console.log(title);
+
+
+const ios =()=>{
+    <TouchableOpacity
+    activeOpacity={0.5}
+    style={[
+        styles.fabLocation,
+        (position ==='bl')? styles.left
+        :styles.right
+    ]}
+      onPress={ onPress  }>
+       <View style={styles.fab}>
+           <Text style={styles.fabText}>{title}</Text>
+       </View>
+   </TouchableOpacity>
+
+}
+
+
+
+const android =()=>{
+
+
     return (//al ser Native Feedback para poder ordenar los botones hay que encapsular todo en un view y agregarle el style de opacity a ese view
        <View 
         style={[styles.fabLocation,
@@ -28,4 +52,6 @@ export const Fab = ({title,onPress,position}:Props) => {
             </TouchableNativeFeedback>
             </View>
     )
+}
+return (Platform.OS ==='ios')? ios() : android();
 }
